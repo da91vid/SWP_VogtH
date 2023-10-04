366008a9e47defb0fe86ccfed87e45b191dfb31d
youngnight91 <david.mark.privat@gmail.com>
2023-10-04 16:05:09 +0200

    Update

--- Web/Web_Grundlagen/Controllers/UserController.cs
+++ Web/Web_Grundlagen/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Web_Grundlagen.Models;
 
 namespace Web_Grundlagen.Controllers
 {
@@ -8,8 +9,31 @@ namespace Web_Grundlagen.Controllers
         {
             return View();
         }
-        public IActionResult SingleUser()
+        public IActionResult Login()
         {
+            return View();
+        }
+        public IActionResult Registration()
+        {
+            return View();
+        }
+        public IActionResult ShowOneUser()
+        {
+            //Daten eines Users vom Kontroller an die View übergeben
+            //Die Userdaten kommen normal aus einer DB
+            User u = new User()
+            {
+                Id = 5,
+                Name = "Red Head Johnny's little Pommes",
+                Birthdate = new DateTime(2004, 02, 04)
+            };
+
+            //User an die View übergeben
+            return View(u);
+        }
+        public IActionResult ShowMultipleUser()
+        {
+            //Daten einer Userliste vom Kontroller an die View übergeben
             return View();
         }
     }
